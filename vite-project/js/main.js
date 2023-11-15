60c502104611e2d9f87c71ce6593a90f9155d328
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-11-15 18:57:47 -0500

    ddd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -9,26 +9,35 @@ const DOMSelectors = {
 
 const microwaveList = foodArray.filter((microwave)=> microwave.microwaveable === false)
 const cheapList = foodArray.filter((money)=> money.price < 5)
-
+function microwaveableie(x){
+  if (x === true){
+    return 'YES'
+  }else if
+  (x === false){
+    return 'NOT'
+  }
+}
 function clear(){
   DOMSelectors.flexblaxs.innerHTML = ''
 }
 DOMSelectors.microwaveCheckbox.addEventListener('click', function(e){
   e.preventDefault();
-  clear()
+  clear();
+  
   function microwaveable(){
     microwaveList.forEach((poop)=>{
-      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`<div class = "card">
-      <img src= alt=>
-      <div class=card-content>
-        <h2>
-          Card Heading
-        </h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
-        </p>
-      </div>
-    </div>`)
+      const fart = microwaveableie(poop.microwaveable);
+      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`
+      <div class = card>
+        <div class="card-content">
+          <h2 class=card-title>
+            ${poop.name}
+          </h2>
+          <p class=card-desc>
+          ${poop.meal} for ${poop.price}  and this food is ${fart} microwaveable mmm yummy 🤤
+          </p>
+        </div>
+      </div>`)
     })
   }
   microwaveable();
@@ -39,18 +48,19 @@ DOMSelectors.cheapies.addEventListener('click', function(e){
   clear()
   function cheap(){
     cheapList.forEach((poop)=>{
-      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`<div class = "card">
+      const fart = microwaveableie(poop.microwaveable);
+      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`
+      <div class = card>
+        <div class="card-content">
+          <h2 class=card-title>
+            ${poop.name}
+          </h2>
+          <p class=card-desc>
+          ${poop.meal} for ${poop.price}  and this food is ${fart} microwaveable mmm yummy 🤤
+          </p>
 
-      <div class="card-content">
-        <h2 class=card-title>
-          ${poop.name}
-        </h2>
-        <p class=card-desc>
-        ${poop.meal}
-        ${poop.price}
-        </p>
-      </div>
-    </div>`)
+        </div>
+      </div>`)
     })
   }
   cheap();
