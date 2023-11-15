f2171d82314b0284ec9a560ef38793347a514e99
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-11-15 09:24:49 -0500

    ddd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -9,6 +9,7 @@ const DOMSelectors = {
 
 const microwaveList = foodArray.filter((microwave)=> microwave.microwaveable === false)
 const cheapList = foodArray.filter((money)=> money.price < 5)
+
 function clear(){
   DOMSelectors.flexblaxs.innerHTML = ''
 }
@@ -17,7 +18,17 @@ DOMSelectors.microwaveCheckbox.addEventListener('click', function(e){
   clear()
   function microwaveable(){
     microwaveList.forEach((poop)=>{
-      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`<div class=card><h2 class=card-title>${poop.name}</h2></div>`)
+      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`<div class = "card">
+      <img src= alt=>
+      <div class=card-content>
+        <h2>
+          Card Heading
+        </h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
+        </p>
+      </div>
+    </div>`)
     })
   }
   microwaveable();
@@ -28,7 +39,18 @@ DOMSelectors.cheapies.addEventListener('click', function(e){
   clear()
   function cheap(){
     cheapList.forEach((poop)=>{
-      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`<div class=card><h2 class=card-title>${poop.name}</h2></div>`)
+      DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`<div class = "card">
+
+      <div class="card-content">
+        <h2 class=card-title>
+          ${poop.name}
+        </h2>
+        <p class=card-desc>
+        ${poop.meal}
+        ${poop.price}
+        </p>
+      </div>
+    </div>`)
     })
   }
   cheap();
