ec03310226b7810ff460d6c52b435f657ae6896d
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-11-16 19:08:17 -0500

    dd

---
+++ vite-project/js/selectors.js
@@ -0,0 +1,17 @@
+const DOMSelectors = {
+    form: document.querySelector('#form-first'),
+    flexblaxs: document.querySelector('#flexblox'),
+}
+const ButtonSelectors = [
+    {michaelwave: document.querySelector('#microwaveables')},
+    {cheapies: document.querySelector('#cheapables')},
+    {sides: document.querySelector('#sides'),},
+    {breakfasts: document.querySelector('#breakfast')},
+    {lunchies: document.querySelector('#lunch')},
+    {dinner: document.querySelector('#dinner')},
+    {middleclass: document.querySelector('#midpriced')},
+    {luxury: document.querySelector('#luxury')},
+]
+
+export {DOMSelectors};
+export {ButtonSelectors}; 
