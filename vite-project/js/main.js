b95c51ee666b3baabddc62f3564ae651474527e6
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-11-16 21:23:09 -0500

    rrr

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,16 +3,16 @@ import { foodArray } from './menu'
 import { DOMSelectors } from './selectors'
 import { ButtonSelectors } from './selectors'
 
-const filteredLists = [
-  {microwaveList: foodArray.filter((microwave)=> microwave.microwaveable === false)},
-  {cheapList: foodArray.filter((money)=> money.price < 5)},
-  {sideList: foodArray.filter((side)=> side.meal === 'Side')},
-  {breakfastList: foodArray.filter((breakfast)=> breakfast.meal === 'Breakfast')},
-  {lunchList: foodArray.filter((lunch)=> lunch.meal === 'Lunch')},
-  {dinnerList: foodArray.filter((dinner)=> dinner.meal === 'Dinner')},
-  {middleClassList: foodArray.filter((middleClass)=> middleClass.price >= 4.99 && middleClass.price <= 9.99)},
-  {expensiveList: foodArray.filter((luxury)=>luxury.price >= 9.99)},
-]
+const filteredLists = {
+  microwaveList: foodArray.filter((microwave)=> microwave.microwaveable === false),
+  cheapList: foodArray.filter((money)=> money.price < 5),
+  sideList: foodArray.filter((side)=> side.type === 'Side'),
+  breakfastList: foodArray.filter((breakfast)=> breakfast.meal === 'Breakfast'),
+  lunchList: foodArray.filter((lunch)=> lunch.meal === 'Lunch'),
+  dinnerList: foodArray.filter((dinner)=> dinner.meal === 'Dinner'),
+  middleClassList: foodArray.filter((middleClass)=> middleClass.price >= 4.99 && middleClass.price <= 9.99),
+  expensiveList: foodArray.filter((luxury)=>luxury.price >= 9.99),
+}
 
 function microwaveableie(x){
   if (x === true){
@@ -36,7 +36,8 @@ function completeList(btn,filteredList){
         const fart = microwaveableie(poop.microwaveable);
         DOMSelectors.flexblaxs.insertAdjacentHTML('beforeend',`
         <div class = card>
-          <div class="card-content">
+          <img src=${poop.img} class=card-img>
+          <div class=card-content>
             <h2 class=card-title>
               ${poop.name}
             </h2>
@@ -50,5 +51,13 @@ function completeList(btn,filteredList){
     global();
   })
 }
+completeList(ButtonSelectors.allbuh,foodArray)
+completeList(ButtonSelectors.breakfasts,filteredLists.breakfastList)
+completeList(ButtonSelectors.michaelwave,filteredLists.microwaveList)
+completeList(ButtonSelectors.cheapies,filteredLists.cheapList)
+completeList(ButtonSelectors.sides,filteredLists.sideList)
+completeList(ButtonSelectors.lunchies,filteredLists.lunchList)
+completeList(ButtonSelectors.dinner,filteredLists.dinnerList)
+completeList(ButtonSelectors.middleclass,filteredLists.middleClassList)
+completeList(ButtonSelectors.luxury,filteredLists.expensiveList)
 
-completeList(ButtonSelectors[4],filteredLists[0])
--- vite-project/js/selectors.js
+++ vite-project/js/selectors.js
@@ -2,16 +2,17 @@ const DOMSelectors = {
     form: document.querySelector('#form-first'),
     flexblaxs: document.querySelector('#flexblox'),
 }
-const ButtonSelectors = [
-    {michaelwave: document.querySelector('#microwaveables')},
-    {cheapies: document.querySelector('#cheapables')},
-    {sides: document.querySelector('#sides'),},
-    {breakfasts: document.querySelector('#breakfast')},
-    {lunchies: document.querySelector('#lunch')},
-    {dinner: document.querySelector('#dinner')},
-    {middleclass: document.querySelector('#midpriced')},
-    {luxury: document.querySelector('#luxury')},
-]
+const ButtonSelectors = {
+    allbuh: document.querySelector('#all'),
+    michaelwave: document.querySelector('#microwaveables'),
+    cheapies: document.querySelector('#cheapables'),
+    sides: document.querySelector('#sides'),
+    breakfasts: document.querySelector('#breakfast'),
+    lunchies: document.querySelector('#lunch'),
+    dinner: document.querySelector('#dinner'),
+    middleclass: document.querySelector('#midpriced'),
+    luxury: document.querySelector('#luxury'),
+}
 
 export {DOMSelectors};
 export {ButtonSelectors}; 
